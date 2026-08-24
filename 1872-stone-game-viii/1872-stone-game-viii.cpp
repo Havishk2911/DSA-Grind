4e5fbd1638ca5245b52cc9e061933b4ed58c03c4
Havishk gor <havishkgor@gmail.com>
2026-08-24 08:28:08 +0530

    Time: 95 ms (89.02%), Space: 95.3 MB (49.63%), Solved in Language: .cpp

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        vector<long long> prefixSum(n);
+        prefixSum[0] = stones[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + stones[i];
+        }
+         long long dp = prefixSum[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            dp = max(dp, prefixSum[i] - dp);
+        }
+
+        return dp;
+    }
+};
